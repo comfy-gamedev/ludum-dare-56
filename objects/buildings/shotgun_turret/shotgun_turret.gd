1ea5c429f8b01da8a8d7d4f51ad16f2802396b27
WiredOverload <mike.everywhere@gmail.com>
2024-10-05 05:37:45 -0500

    added three more turrets

---
+++ objects/buildings/shotgun_turret/shotgun_turret.gd
@@ -0,0 +1,47 @@
+extends Building
+
+var target := Vector2(1, 0)
+
+@onready var head := $Head
+@onready var cooldown := $Timer
+@export var damage = 6
+@export var pellets = 5
+
+var rock_scene = preload("res://objects/buildings/rock_turret/rock.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_health = 50
+	health = max_health
+	reach = 100
+	if team == Enums.Team.RED:
+		$Base.material = red_material
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var enemies := get_tree().get_nodes_in_group("Unit").filter(func(x): return x.team != team)
+	var targets = enemies.map(func(x): return x.global_position)
+	
+	target = Vector2(10000, 0)
+	var dist
+	var min_dist = 10000.0
+	for i in targets:
+		dist = global_position.distance_squared_to(i)
+		if dist < min_dist:
+			min_dist = dist
+			target = i
+	
+	head.rotation = global_position.angle_to_point(target) + (PI/2)
+	
+	if global_position.distance_to(target) < reach && cooldown.is_stopped():
+		head.play()
+		var angle_offset = -PI/6
+		for i in pellets:
+			var rock = rock_scene.instantiate()
+			get_parent().add_child(rock)
+			rock.global_position = global_position
+			rock.velocity = (target - global_position).normalized().rotated(angle_offset) * 50
+			angle_offset += PI/12
+			rock.damage = damage
+			rock.team = team
+		cooldown.start()
