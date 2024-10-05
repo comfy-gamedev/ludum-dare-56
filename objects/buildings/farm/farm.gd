e6fb80782afab7a09a54f350e132b4ff56170993
WiredOverload <mike.everywhere@gmail.com>
2024-10-05 08:49:16 -0500

    added start of farm building

---
+++ objects/buildings/farm/farm.gd
@@ -0,0 +1,20 @@
+extends Building
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_health = 50
+	health = max_health
+	if team == Enums.Team.RED:
+		$Sprite2D.material = red_material
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	#gain money/mana
+	#play squish animation? put a floating +1 in front of it? play fireworks?
+	pass # Replace with function body.
