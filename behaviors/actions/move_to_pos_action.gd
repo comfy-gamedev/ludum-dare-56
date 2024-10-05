bc8b0f451d135d9ff7ff38ae1baf58faee565fe0
jjwall <jacob.wallace@utexas.edu>
2024-10-04 19:48:03 -0500

    flip sprite if it exists

--- behaviors/actions/move_to_pos_action.gd
+++ behaviors/actions/move_to_pos_action.gd
@@ -6,15 +6,22 @@ extends ActionLeaf
 # This action moves the actor towards a given target pos (new_pos in Blackboard).
 # Note: This action should always be the child of a TimeLimiterDecorator in the event 
 # that the actor is unable to meet the acceptance_radius.
-# Note2: This should be fish specific movement.
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var target_pos = blackboard.get_value("new_pos")
 	var distance_to_target = blackboard.get_value("distance_to_target")
 	var delta = get_physics_process_delta_time()
 	
 	# Calculate the direction to the target position
-	var direction = (target_pos - actor.position).normalized()
+	var direction: Vector2 = (target_pos - actor.position).normalized()
 	var current_distance_to_target = actor.position.distance_to(target_pos)
+	
+	# Flip sprite if it exists.
+	var spriteNode = actor.get_node_or_null("AnimatedSprite2D")
+	if spriteNode:
+		if direction.x > 0:
+			spriteNode.flip_h = true
+		else:
+			spriteNode.flip_h = false
 	
 	if current_distance_to_target > distance_to_target/2:
 		actor.velocity = move_speed * direction
