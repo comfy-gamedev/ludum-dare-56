c69beb38b52700fdb59cccb5bf59011399eac935
jjwall <jacob.wallace@utexas.edu>
2024-10-05 00:09:31 -0500

    attack units is working

--- behaviors/actions/detect_target_action.gd
+++ behaviors/actions/detect_target_action.gd
@@ -4,7 +4,8 @@ extends ActionLeaf
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	var opponents = blackboard.get_value("nearby_opponents")
 	
-	if opponents and opponents.size() > 0:
+	if opponents != null and opponents.size() > 0:
+		print("heeeeeyyyy")
 		var target_opponent = opponents.pick_random()
 		blackboard.set_value("target_opponent", target_opponent)
 		return SUCCESS
