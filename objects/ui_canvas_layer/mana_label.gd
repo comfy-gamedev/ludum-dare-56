411436b8f025707d4ed97848e43e91cab5c65413
WiredOverload <mike.everywhere@gmail.com>
2024-10-05 23:50:52 -0500

    added economy

---
+++ objects/ui_canvas_layer/mana_label.gd
@@ -0,0 +1,11 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = "Mana : " + str(Globals.player_money)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	text = "Mana : " + str(Globals.player_money)
