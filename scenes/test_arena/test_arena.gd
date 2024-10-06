84f9a86eb508527ea41d3ae633f3633035fbc3d6
jjwall <jacob.wallace@utexas.edu>
2024-10-06 01:28:36 -0500

    building refactor

--- scenes/test_arena/test_arena.gd
+++ scenes/test_arena/test_arena.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$RockTurret.on_day()
+	$LaserTurret.on_day()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
