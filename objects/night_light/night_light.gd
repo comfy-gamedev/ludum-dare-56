55223b064c2b7de21d988ff911b1fb67ccf2c79b
Apples <2352020+apples@users.noreply.github.com>
2024-10-06 05:55:20 -0500

    Best lights

--- objects/night_light/night_light.gd
+++ objects/night_light/night_light.gd
@@ -5,10 +5,17 @@ extends Node2D
 
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
 @onready var light: Node2D = $CanvasLayer/Light
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
 	process_mode = PROCESS_MODE_ALWAYS
 	canvas_layer.custom_viewport = sub_viewport
+	timer.start(randf_range(0.1, 0.2))
 
 func _process(delta: float) -> void:
 	light.global_transform = global_transform
+
+func _on_timer_timeout() -> void:
+	var f = randf_range(1.0, 1.1)
+	light.flicker = move_toward(light.flicker, f, 0.05)
+	timer.start(randf_range(0.3, 0.4))
