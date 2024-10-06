780a304bfc940b5d00eea141f1a5154ab8555272
WiredOverload <mike.everywhere@gmail.com>
2024-10-06 07:23:03 -0500

    enemies target closest

--- behaviors/actions/select_building_target_action.gd
+++ behaviors/actions/select_building_target_action.gd
@@ -6,7 +6,15 @@ func tick(actor: Node, blackboard: Blackboard) -> int:
 	var enemy_buildings = buildings.filter(func(x): return x.team != actor.team)
 	
 	if enemy_buildings.size() > 0:
-		blackboard.set_value("target_building", enemy_buildings.pick_random())
+		var target
+		var dist
+		var min_dist = 1000000.0
+		for i in enemy_buildings:
+			dist = actor.global_position.distance_squared_to(i.global_position)
+			if dist < min_dist:
+				min_dist = dist
+				target = i
+		blackboard.set_value("target_building", target)
 		
 	return SUCCESS
 	#else:
