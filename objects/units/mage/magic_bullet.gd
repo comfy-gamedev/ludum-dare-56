8dafd1a4410bb3f95f5c4c3d6342137e33ad16f5
jjwall <jacob.wallace@utexas.edu>
2024-10-06 17:50:54 -0500

    fixed magic bullet collision with wired's mask and layer changes

--- objects/units/mage/magic_bullet.gd
+++ objects/units/mage/magic_bullet.gd
@@ -1,8 +1,16 @@
-extends Node2D
+extends Area2D
 
 var team: Enums.Team = Enums.Team.BLUE
 var velocity := Vector2(5, 0)
 var damage := 10
+
+func _ready() -> void:
+	if team == Enums.Team.BLUE:
+		collision_layer = 0
+		collision_mask = 0b10100
+	elif team == Enums.Team.RED:
+		collision_layer = 0
+		collision_mask = 0b01010
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +19,6 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print("1234", "HITT ???")
 	if (body.is_in_group("Unit") or body.is_in_group("Building")) and team != body.team:
 		body.hit(damage)
 		queue_free()
