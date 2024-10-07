7dc18008b95c988d1048264e8fae8de5c6e9ddc5
Apples <2352020+apples@users.noreply.github.com>
2024-10-06 22:43:57 -0500

    Fast-forward

--- objects/ui_canvas_layer/ui_canvas_layer.gd
+++ objects/ui_canvas_layer/ui_canvas_layer.gd
@@ -11,6 +11,7 @@ var upgrades_panel: Control
 
 @onready var hotbar_panel_container: PanelContainer = $Control/HotbarPanelContainer
 @onready var start_battle_button: Button = $Control/StartBattleButton
+@onready var ff_button: Button = $Control/FFButton
 
 func _ready() -> void:
 	if is_upgrade_screen:
@@ -20,6 +21,8 @@ func _ready() -> void:
 		$Control.add_child(upgrades_panel)
 		upgrades_panel.done.connect(_on_upgrades_panel_done)
 		start_battle_button.queue_free()
+	else:
+		Globals.phase_changed.connect(_on_phase_changed)
 
 func _on_upgrades_panel_done() -> void:
 	upgrades_panel.queue_free()
@@ -28,3 +31,19 @@ func _on_upgrades_panel_done() -> void:
 ## async
 func choose_hotbar_slot() -> int:
 	return await upgrades_panel.choose_hotbar_slot()
+
+func _on_phase_changed() -> void:
+	if Globals.phase != Enums.Phase.FIGHT:
+		Engine.time_scale = 1.0
+		ff_button.hide()
+	else:
+		ff_button.text = "FF >>"
+		ff_button.show()
+
+func _on_ff_button_pressed() -> void:
+	if Engine.time_scale == 1.0:
+		ff_button.text = "FF <"
+		Engine.time_scale = 2.0
+	else:
+		ff_button.text = "FF >>"
+		Engine.time_scale = 1.0
