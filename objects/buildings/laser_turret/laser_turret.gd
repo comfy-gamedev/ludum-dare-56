13c4f7fe8e5b0e30af4e8d0ff2c42f427e4f6558
Apples <2352020+apples@users.noreply.github.com>
2024-10-07 04:17:59 -0500

    Laser fx

--- objects/buildings/laser_turret/laser_turret.gd
+++ objects/buildings/laser_turret/laser_turret.gd
@@ -8,8 +8,12 @@ var target_node
 
 var laser_visible = false
 
+var rd_t: float = 0.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	rd_t += delta
+	
 	var enemies := get_tree().get_nodes_in_group("Unit").filter(func(x): return x.team != team)
 	if !enemies:
 		laser_visible = false
@@ -31,11 +35,30 @@ func _process(delta: float) -> void:
 		head.play()
 		target_node.hit(damage * delta)
 		laser_visible = true
-		queue_redraw()
+		_maybe_queue_redraw()
 	else:
 		laser_visible = false
 		queue_redraw()
 
+func _maybe_queue_redraw() -> void:
+	if rd_t >= 0.1:
+		rd_t -= 0.1
+		if rd_t >= 0.1:
+			rd_t = 0.0
+		queue_redraw()
+
+func _draw_bzzt(to: Vector2, color: Color) -> void:
+	var n = maxf(1, roundi(to.length() / 8.0))
+	var a = Vector2.ZERO
+	for i in range(1, n+1):
+		var b = to * float(i) / float(n)
+		b += Vector2.from_angle(randf_range(0.0, TAU)) * 4.0
+		draw_line(a, b, color, 2.0)
+		a = b
+
 func _draw() -> void:
 	if target_node and laser_visible:
-		draw_line(Vector2(0, 0), to_local(target_node.global_position), Color.CHARTREUSE, 2.0)
+		var d = to_local(target_node.global_position)
+		_draw_bzzt(d, Color("ac3232"))
+		_draw_bzzt(d, Color("d95763"))
+		_draw_bzzt(d, Color("d77bba"))
