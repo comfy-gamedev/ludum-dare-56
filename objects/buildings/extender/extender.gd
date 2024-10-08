d6c3720bd5585df583e5db72c7a97a6a2e3aa625
WiredOverload <mike.everywhere@gmail.com>
2024-10-07 21:33:23 -0500

    fixed extender sfx

--- objects/buildings/extender/extender.gd
+++ objects/buildings/extender/extender.gd
@@ -8,6 +8,8 @@ func _ready() -> void:
 
 func hit(damage_taken: float):
 	health -= damage_taken
+	MusicMan.sfx(preload("res://assests/SFX/hit.wav"), "hit", 1)
 	if health <= 0:
+		MusicMan.sfx(preload("res://assests/SFX/destroy-building.wav"), "destroy")
 		grid_manager.remove_castle(id)
 		queue_free()
